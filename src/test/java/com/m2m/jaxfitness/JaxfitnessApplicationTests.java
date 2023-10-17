9699b2e8425bd83c3d92d0b987c96bed874ad06e
Jonghwan Won <anarose4815@gmail.com>
2023-10-17 21:31:16 +0900

    git push test backend

---
+++ src/test/java/com/m2m/jaxfitness/JaxfitnessApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m2m.jaxfitness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JaxfitnessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
